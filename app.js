ee7ce6a87e7d5cb5635c658293c9453452264658
Marian Aphiare <140634802+marykevwe@users.noreply.github.com>
2023-09-28 09:00:13 -0400

    update app.js code

--- app.js
+++ app.js
@@ -8,5 +8,29 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         list.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    inputBox.value = "";
+    saveDate();
 }
+list.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveDate();
+
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveDate();
+    }
+}, false);
+
+function saveDate(){
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showtask(){
+    list.innerHTML = localStorage.getItem("data");
+}
+showtask();
