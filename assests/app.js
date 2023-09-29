43d7a975c0041beaa5b7177b723ef3a32a7293de
Marian Aphiare <140634802+marykevwe@users.noreply.github.com>
2023-09-29 06:21:59 -0400

    add assests folder

--- assests/app.js
+++ assests/app.js
@@ -5,32 +5,39 @@ function addTask() {
     if (inputBox.value === '') {
         alert("You must write something");
     } else {
-        let li = document.createElement("li");
+        const li = document.createElement("li");
         li.innerHTML = inputBox.value;
-        list.appendChild(li);
-        let span = document.createElement("span")
+
+        const span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+
+        list.appendChild(li);
+
+        inputBox.value = "";
+        saveData();
     }
-    inputBox.value = "";
-    saveDate();
 }
-list.addEventListener("click", function(e) {
+
+list.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
-
     } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-        saveDate();
+        saveData();
     }
 }, false);
 
-function saveData(){
-    localStorage.setItem("data", list.innerHTML);
+function saveData() {
+    localStorage.setItem("taskList", list.innerHTML);
 }
 
-function showtask(){
-    list.innerHTML = localStorage.getItem("data");
+function showTasks() {
+    const storedData = localStorage.getItem("taskList");
+    if (storedData) {
+        list.innerHTML = storedData;
+    }
 }
-showtask();
+
+showTasks();
